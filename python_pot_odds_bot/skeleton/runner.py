a7f153c8ff9defb26c361cde1097c9436a8d63e9
Spencer Lin <spencerlin@Spencers-MacBook-Pro-2.local>
2024-01-11 23:50:09 -0500

    made changes in runner.py to update engine

--- python_pot_odds_bot/skeleton/runner.py
+++ python_pot_odds_bot/skeleton/runner.py
@@ -85,7 +85,7 @@ class Runner():
                     bids = bids.split(',')
                     stacks = stacks.split(',')
                     hands[active] = active_hands.split(',')
-                    round_state = RoundState(round_state.button, round_state.street, round_state.auction, [int(x) for x in bids], round_state.pips, [int(x) for x in stacks], hands, [], round_state)
+                    round_state = RoundState(round_state.button, round_state.street, round_state.auction, [int(x) for x in bids], round_state.pips, [int(x) for x in stacks], hands, round_state.deck, round_state)
                 elif clause[0] == 'B':
                     round_state = RoundState(round_state.button, round_state.street, round_state.auction, round_state.bids, 
                                             round_state.pips, round_state.stacks, round_state.hands, clause[1:].split(','), 
